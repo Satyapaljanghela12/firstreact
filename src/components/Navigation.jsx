2c59fba84a63a5ef71716a00db265b63b0ce1e10
Satyapaljanghela12 <satyapaljanghela@gmail.com>
2025-01-27 22:27:46 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <h1>Books.<span>io</span></h1>
+        
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button className="login">login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
+
